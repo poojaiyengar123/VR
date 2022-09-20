0085a6c6cfd0fa4ad26054e43dcafbba637befc8
poojaiyengar123 <poojaiyengar68@gmail.com>
2022-09-20 08:10:03 -0400

    moved dictionary and equilateral polygon

---
+++ DIctionary/DIctionary/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> teachers = new Dictionary<string, string>();
+            teachers.Add("Data Structures", "Dentler");
+            teachers.Add("VR", "Trainor");
+            teachers.Add("Psychology", "McGinley");
+            teachers.Add("Artificial Intelligence", "Schiff");
+            teachers.Add("Gym", "Boehm");
+            teachers.Add("Math", "King");
+            teachers.Add("French", "Ghobrial");
+            teachers.Add("English", "Noebels");
+            teachers.Add("Science", "Burnett");
+            teachers.Add("Art", "Castaldo");
+
+            FindSubjects(teachers, "Science");
+            FindSubjects(teachers, "Math");
+            FindSubjects(teachers, "Art");
+        }
+        static void FindSubjects(Dictionary<string, string> teachers, string subject)
+        {
+            foreach(KeyValuePair<string, string> kvp in teachers)
+            {
+                if (kvp.Key == subject)
+                    Console.WriteLine(subject + ": " + kvp.Value);
+            }
+        }
+    }
+}
---
+++ EquilateralPolygon/EquilateralPolygon/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace EquilateralPolygon
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square();
+            string value = square.ToString();
+            Console.WriteLine(value);
+        }
+    }
+    public class EquilateralPolygon
+    {
+        public int sides = 4;
+        public int sideLengths = 5;
+    }
+    public class Square : EquilateralPolygon
+    {
+        int area;
+        int perimeter;
+
+        public int GetArea()
+        {
+            area = sideLengths * sideLengths;
+            return area;
+        }
+        public int GetPerimeter()
+        {
+            perimeter = sides * sideLengths;
+            return perimeter;
+        }
+        public string ToString()
+        {
+            return "Area: " + GetArea() + ", Perimeter: " + GetPerimeter();
+        }
+    }
+}
